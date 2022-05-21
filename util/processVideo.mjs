64a37c65ad9e0e6370d3dac808401ecc5a136956
Welton Felix <contato.weltonf@gmail.com>
2022-05-20 22:41:23 -0300

    Adds spinner indicator

--- util/processVideo.mjs
+++ util/processVideo.mjs
@@ -1,5 +1,5 @@
 import { path, chalk } from 'zx';
-import { echo } from 'zx/experimental';
+import { echo, startSpinner } from 'zx/experimental';
 
 import { handleMkv, handleMp4 } from './formatHandlers.mjs';
 
@@ -14,15 +14,18 @@ export default function processVideo({
   const outFile = path.resolve(outDir, file);
 
   echo(chalk.blue(`Processing '${file}'...`));
+  const stopSpinner = startSpinner(`Processing videos...`);
   switch (extension) {
     case 'mp4':
       handleMp4(inputVideo, inputDir, outFile, subtitles).then(() => {
         echo(chalk.green(`${file} processed.`));
+        stopSpinner();
       });
       break;
     case 'mkv':
       handleMkv(inputVideo, inputDir, outFile, subtitles).then(() => {
         echo(chalk.green(`${file} processed.`));
+        stopSpinner();
       });
       break;
     default:
